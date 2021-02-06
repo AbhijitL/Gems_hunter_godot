952d8c69a3deec7d972053fbe4fa5eaf7c6fe761
ratatata <labhijitsingh13@gmail.com>
2021-02-06 16:09:01 +0530

    added moving platform and a basic enemy

--- Scenes/Enemy/Enemy.gd
+++ Scenes/Enemy/Enemy.gd
@@ -1,6 +1,9 @@
-extends Node2D
+extends KinematicBody2D
 class_name Enemy
 var damage_points : int = 1;
+
+export(int) var Max_Speed = 15;
+var motion 
 
 func _on_Area2D_body_entered(body):
 	if body.get_name() == "Player":
--- main.gd
+++ main.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 onready var intro_timer : Timer = $IntroTimer;
+onready var cutscene_timer : Timer = $CutsceneTimer;
 onready var intro = $ViewportContainer/Viewport/Intro;
 onready var cont_screen = $ViewportContainer/Viewport/Continue;
 onready var menu = $ViewportContainer/Viewport/Menu;
 onready var play_button = $ViewportContainer/Viewport/Menu/Control/VBoxContainer2/HBoxContainer/VBoxContainer/Play;
+onready var cutscene = $ViewportContainer/Viewport/Intro_Entrance;
 
 func _ready():
 	_start_up();
@@ -15,14 +17,17 @@ func _start_up():
 
 func _on_IntroTimer_timeout():
 	intro.visible = false;
-	cont_screen.visible = true
+	$ViewportContainer/Viewport/Intro_Entrance.visible = true;
+	$CutsceneTimer.start();
+	
 	
 func _input(event):
 	if not menu.visible:
-		if event.is_action_pressed("ui_accept") and not intro.visible:
+		if event.is_action_pressed("ui_accept") and not intro.visible and not cutscene.visible:
 			cont_screen.visible = false;
 			menu.visible = true;play_button.grab_focus();
-		
 
 
-
+func _on_CutsceneTimer_timeout():
+	cont_screen.visible = true;
+	cutscene.visible = false;
